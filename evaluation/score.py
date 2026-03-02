33eae383515563521a052e3b5fac1037f65d4601
Daouda-sene <daouda.sene@aims-senegal.org>
2026-03-02 12:49:59 +0100

    Add script to calculate and save accuracy score

--- evaluation/score.py
+++ evaluation/score.py
@@ -1,1 +1,19 @@
+import numpy as np
+import json
+import sys
 
+if len(sys.argv) < 2:
+    print("Submission file missing")
+    sys.exit(1)
+
+submission_path = sys.argv[1]
+preds = np.loadtxt(submission_path)
+
+y_true = np.array([0,1,1,0])
+
+accuracy = np.mean(preds == y_true)
+
+with open("score.json", "w") as f:
+    json.dump({"accuracy": float(accuracy)}, f)
+
+print("Accuracy:", accuracy)
