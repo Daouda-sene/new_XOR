9b28c2c71cacb132405dbe5717adfe37129e6b6b
Daouda-sene <daouda.sene@aims-senegal.org>
2026-03-02 15:49:21 +0100

    Rename XORNet class to Net and update super call

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class XORNet(nn.Module):
+class Net(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(Net, self).__init__()
         self.fc1 = nn.Linear(2, 8)
         self.fc2 = nn.Linear(8, 1)
 
@@ -11,4 +11,3 @@ class XORNet(nn.Module):
         x = torch.tanh(self.fc1(x))
         x = torch.sigmoid(self.fc2(x))
         return x
-
